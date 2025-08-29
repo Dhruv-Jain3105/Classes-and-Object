e5f1e8362c1e426c49c24a8de5084ad34c2ae763
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-08-29 11:32:08 +0530

    Create Basic_classes.cpp

---
+++ Basic_classes.cpp
@@ -0,0 +1,23 @@
+/*
+Name:- Dhruv Jain
+PRN:-24070123042
+Batch:-A2
+*/
+#include <iostream>
+using namespace std;
+class Cube{
+    public:
+    int height=5;
+    int width=3;
+    int length=4;
+};
+int main(){
+    Cube x1;
+    int vol=x1.height*x1.width*x1.length;
+    cout << "Volume of the cube is: " << vol << endl;
+    return 0;
+}
+/*
+Output:-
+Volume of the cube is: 60
+*/
